73f2fbc775458a0542a29b928fd8868d317694e2
simungiltia-gif <simungiltia@gmail.com>
2025-09-16 22:01:09 +0700

    Add files via upload

---
+++ Tugas1pelcod.py
@@ -0,0 +1,14 @@
+# I.	Nama lengkap mahasiswa: String
+# Benar. karena String adalah tipe data yang berisi huruf,angka,dan simbol
+
+# II.	Nomor Induk Mahasiswa (NIM): String
+# Jika kita menggunakan Integer(INT) makan NIM yang berawalan angka 0 akan ter-skip ke angka berikutnya, contoh jika kita input NIM 08214269 maka di dalam variable nilainya bukan bernilai 08214269 melainkan 8214269 (tidak ada angka 0), jadimaka tipe data untuk NIM lebih aman menggunakan String
+
+# III.	Jumlah SKS yang sudah di ambil: Integer
+# Karena jumlah SKS menggunakan bilangan bulat, bukan decimal. dan juga tidak ada jumlah SKS yang bernilai 24.5
+
+# IV.	Status keaktifan (aktif / tidak aktif): Boolean
+# Benar karena hanya tipe data Boolean yg memiliki nilai YA/TIDAK
+
+# V.	IPK saat ini: Float
+# Karena IPK memiliki nilai decimal, contoh IPK teman saya 3.7
